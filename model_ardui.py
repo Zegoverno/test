cab32f272e259ff1fd4292b83aea3bf304add736
José Governo <zegoverno@hotmail.com>
2017-11-21 23:03:17 -0200

    Update model_ardui.py

--- model_ardui.py
+++ model_ardui.py
@@ -1,5 +1,5 @@
 from ardui_server import db
 
 class Medida(db.Model):
-	id		= db.Column(db.Integer, primary_key=True)
-	valor	= db.Column(db.Integer)
+	id = db.Column(db.Integer, primary_key=True)
+	valor = db.Column(db.Integer)
